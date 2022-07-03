7926e9f297f46f956bf77e3edde706cc1f997b92
webxf <j17852533489@163.com>
2022-07-03 23:05:44 +0800

    完成作业

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todoapp">
+    <TodoHeader @add="addFn"></TodoHeader>
+    <TodoMain :list="showList" @delFn="delFnn"></TodoMain>
+    <TodoFooter
+      :count="count"
+      @changeFn="changeFnn"
+      @clear="clearFn"
+    ></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from './components/TodoHeader.vue';
+import TodoMain from './components/TodoMain.vue';
 export default {
-  name: 'App',
+  data() {
+    return {
+      list: JSON.parse(localStorage.getItem('list')) || [],
+      getSel: 'all',
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
+  },
+  methods: {
+    addFn(val) {
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({
+        id,
+        name: val,
+        isDone: false,
+      });
+    },
+    delFnn(val) {
+      const index = this.list.findIndex((ele) => (ele.id = val));
+      this.list.splice(index, 1);
+    },
+    changeFnn(val) {
+      this.getSel = val;
+    },
+    clearFn() {
+      this.list.forEach((ele) => (ele.isDone = false));
+    },
+  },
+  computed: {
+    count() {
+      return this.list.filter((ele) => !ele.isDone).length;
+    },
+    showList() {
+      if (this.getSel == 'no') {
+        return this.list.filter((ele) => !ele.isDone);
+      } else if (this.getSel == 'yes') {
+        return this.list.filter((ele) => ele.isDone);
+      } else {
+        return this.list;
+      }
+    },
+  },
+  watch: {
+    list: {
+      deep: true,
+      handler(val) {
+        localStorage.setItem('list', JSON.stringify(val || []));
+      },
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import './assets/styles/base.css';
+import './assets/styles/index.css';
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
